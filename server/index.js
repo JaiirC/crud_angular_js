2542d28e33926ca496cfaa0d1f52cf3e4b7aa2e9
JaiirC <jairskater02@hotmail.com>
2022-03-04 18:10:10 -0500

    40,58

--- server/index.js
+++ server/index.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const morgan =require('morgan');
 const app = express();
 // settings
 
 app.set('port',process.env.PORT || 3000)
 
 // Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+
 
 //routes
 
 //start server
 
 app.listen(app.get('port'), () => {
-    console.log('Server on port 3000');
+    console.log('Server on port', app.get('port'));
 });
